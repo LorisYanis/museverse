97dd8f8f6bd07e57ac578b5930ece0c8a0c0ad35
LorisYanis <irvingyanis@gmail.com>
2023-11-12 18:52:47 +0200

    added tailwind prettier plugin and improved the gradient

--- app/(landing)/(routes)/page.tsx
+++ app/(landing)/(routes)/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
     <div className="flex-1 flex flex-col gap-10 items-center justify-center">
       <div className="flex flex-col gap-5 items-center justify-center">
         <div className="flex flex-col gap-4 items-center justify-center h-full">
-          <h1 className="text-4xl md:text-6xl text-transparent bg-clip-text font-black bg-gradient-to-r from-zinc-400 via-zinc-200 to-white background-animate">
+          <h1 className="text-4xl md:text-6xl text-transparent bg-clip-text font-black bg-gradient-to-r from-zinc-500 via-zinc-300 to-white background-animate">
             museverse.ai
           </h1>
           <p className="text-lg text-zinc-300">
@@ -37,7 +37,7 @@ export default function Home() {
       </div>
       <FakeChat />
       <div>
-        <div className="-z-50 translate-x-1/2 bg-gradient-to-t from-zinc-200 via-zinc-600 to-zinc-900 right-1/2 fixed bottom-0 translate-y-1/2 h-[1000px] w-[1000px] blur-[200px] rounded-full glow-animate" />
+        <div className="-z-50 translate-x-1/2 bg-gradient-to-t from-zinc-400 via-zinc-700 to-zinc-900 right-1/2 fixed bottom-0 translate-y-1/2 h-[1000px] w-[1000px] blur-[200px] rounded-full glow-animate background-animate" />
       </div>
     </div>
   );
