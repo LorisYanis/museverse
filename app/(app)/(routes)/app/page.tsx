960f1e920d014fcd5ef055e94a52630c0b62c23c
0Empty0 <victor.gargat28@gmail.com>
2023-11-13 11:31:15 +0100

    add responsivness to app page

--- app/(app)/(routes)/app/page.tsx
+++ app/(app)/(routes)/app/page.tsx
@@ -14,26 +14,27 @@ const ACTIVE = "All";
 
 const App = () => {
   return (
-    <div className="flex-1 flex-col">
+    <div className="flex flex-1 flex-col items-center">
+      <nav className="scroll-bar mt-16 mb-10 bg-zinc-900 p-1 rounded-lg max-md:w-full max-md:px-2 max-md:mt-10">
+        <ul className="space-x-2 font-medium text-sm max-md:overflow-x-scroll max-md:whitespace-nowrap">
+          {CATEGORIES.map((category, index) => (
+            <li
+              className={cn(
+                "relative inline-block px-4 py-2 rounded-lg text-zinc-400 hover:bg-zinc-950 hover:text-white transition-colors",
+                category === ACTIVE ? "bg-zinc-950 text-white z-20" : "",
+              )}
+              key={index}
+            >
+              <button type="button">{category}</button>
+            </li>
+          ))}
+        </ul>
+      </nav>
+
       <section className="mb-36">
         <div className="container">
           <div className="flex flex-col items-center">
-            <nav className="mt-16 mb-10 bg-zinc-900 p-1 rounded-lg">
-              <ul className="space-x-2 font-medium text-sm">
-                {CATEGORIES.map((category, index) => (
-                  <li
-                    className={cn(
-                      "inline-block px-4 py-2 rounded-lg text-zinc-400 hover:bg-zinc-950 hover:text-white transition-colors",
-                      category === ACTIVE ? "bg-zinc-950 text-white" : "",
-                    )}
-                    key={index}
-                  >
-                    <button type="button">{category}</button>
-                  </li>
-                ))}
-              </ul>
-            </nav>
-            <div className="grid grid-cols-4 gap-9">
+            <div className="grid grid-cols-4 max-xl:grid-cols-3 max-lg:grid-cols-2 max-md:gap-4 max-sm:grid-cols-1 gap-9">
               <ChatCard
                 image="/images/thumbnail.png"
                 title="Steve Jobs"
