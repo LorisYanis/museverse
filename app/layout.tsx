6c22c86d5a36a9cc1a4ab664f52211b9818d4711
0Empty0 <victor.gargat28@gmail.com>
2023-11-23 22:00:53 +0100

    configure viewport attrebute

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,6 +40,12 @@ export const metadata: Metadata = {
   },
 };
 
+export const viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+};
+
 export default function RootLayout({
   children,
 }: {
